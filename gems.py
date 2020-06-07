0df8b80e10ceb787924e96cab66d10a1c3f09745
Juan Ignacio Rodriguez de Leon <euribates@gmail.com>
2020-06-07 20:53:21 +0100

    Gems unsorted

--- gems.py
+++ gems.py
@@ -1,18 +1,15 @@
 gems = {
-    "Power": "Marvel Team-Up #55 (Mar. 1977)",
-    "Mind": "Captain Marvel #41 (Nov. 1975)",
-    "Time": "Marvel Team-Up #55 (Mar. 1977)",
-    "Reality": "Avengers Annual #7 (1977)",
-    "Soul": "Marvel Premiere #1 (April 1972)",
-    "Space": "Avengers Annual #7 (1977)"
-}
-# --------------------------------
-gems = {
-    "Power": "Marvel Team-Up #55 (Mar. 1977)",
-    "Mind": "Captain Marvel #41 (Nov. 1975)",
-    "Time": "Marvel Team-Up #55 (Mar. 1977)",
-    "Reality": "Avengers Annual #7 (1977)",
-    "Soul": "Marvel Premiere #1 (April 1972)",
-    "Space": "Avengers Annual #7 (1977)",
+    "topaz": {"blue"},
+    "jade": {"green", "lavender", "red", "yellow", "white", "black"},
+    "opal": {"white", "gray", "green"},
+    "azurite": {"deep blue"}
 }
 
+# -------------------
+
+gems = {
+    "topaz": {"blue"},
+    "jade": {"green", "lavender", "red", "yellow", "white", "black"},
+    "opal": {"white", "gray", "green"},
+    "azurite": {"deep blue"},
+}
