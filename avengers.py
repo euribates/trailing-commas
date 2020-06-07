8c4ecbc9c6c4104d3b83f4ce92122e40d2b5c833
Juan Ignacio Rodriguez de Leon <euribates@gmail.com>
2020-06-07 20:14:27 +0100

    Avengers example

---
+++ avengers.py
@@ -0,0 +1,15 @@
+Avengers = [
+    "Iron Man",
+    "The Wasp",
+    "Hulk",
+    "Ant-Man"
+]
+
+# -------------------
+
+Avengers = [
+    "Iron Man",
+    "The Wasp",
+    "Hulk",
+    "Ant-Man",
+]
