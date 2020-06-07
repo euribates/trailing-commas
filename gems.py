5ebb81352f5e1bc1077ebd52b4a2c4bf54bb74c2
Juan Ignacio Rodriguez de Leon <euribates@gmail.com>
2020-06-07 20:33:26 +0100

    The Soul gems (Infinity Stones) example

---
+++ gems.py
@@ -0,0 +1,18 @@
+gems = {
+    "Mind": "Captain Marvel #41 (Nov. 1975)",
+    "Power": "Marvel Team-Up #55 (Mar. 1977)",
+    "Reality": "Avengers Annual #7 (1977)",
+    "Soul": "Marvel Premiere #1 (April 1972)",
+    "Space": "Avengers Annual #7 (1977)",
+    "Time": "Marvel Team-Up #55 (Mar. 1977)"
+}
+# --------------------------------
+gems = {
+    "Mind": "Captain Marvel #41 (Nov. 1975)",
+    "Power": "Marvel Team-Up #55 (Mar. 1977)",
+    "Reality": "Avengers Annual #7 (1977)",
+    "Soul": "Marvel Premiere #1 (April 1972)",
+    "Space": "Avengers Annual #7 (1977)",
+    "Time": "Marvel Team-Up #55 (Mar. 1977)",
+}
+
